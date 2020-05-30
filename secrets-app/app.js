55d5d9984060b54a2ffb99cea97768f04fcbd09b
Sourav Saini <ssaini@BLDM2345-MAC.local>
2020-05-30 17:17:13 +0530

    OAuth 2.0 Google Authentication

--- secrets-app/app.js
+++ secrets-app/app.js
@@ -12,6 +12,9 @@ const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const findOrCreate = require('mongoose-findorcreate')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -30,16 +33,33 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true)
 
 //schema
 const userSchema = new mongoose.Schema({
   'email': String,
-  'password': String
+  'password': String,
+  'googleId': String,
+  'secret': String
 })
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 // USED for database encryption mechanism for secrets using environment variables
 //userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
@@ -49,12 +69,30 @@ userSchema.plugin(passportLocalMongoose)
 const User = mongoose.model('User', userSchema)
 
 passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 app.get('/', function(req, res) {
   res.render('home')
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+)
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get('/login', function(req, res) {
   res.render('login')
@@ -65,12 +103,40 @@ app.get('/register', function(req, res) {
 })
 
 app.get('/secrets', function(req, res) {
-  console.log(req.isAuthenticated())
+  User.find({"secret": {$ne:null}}, function(err, users) {
+    if (err) {
+      console.log(err)
+    } else {
+      if (users) {
+        res.render("secrets", {usersWithSecret: users})
+      }
+    }
+  })  //where secret field is not null
+})
+
+app.get('/submit', function(req, res) {
   if (req.isAuthenticated()) {
-    res.render("secrets")
+    res.render("submit")
   } else {
     res.redirect('/login')
   }
+})
+
+app.post('/submit', function(req, res) {
+  const submittedSecret = req.body.secret
+
+  User.findById(req.user.id, function(err, user) {
+    if (err) {
+      console.log(err)
+    } else {
+      if (user) {
+        user.secret = submittedSecret
+        user.save(function() {
+          res.redirect('/secrets')
+        })
+      }
+    }
+  })
 })
 
 app.get('/logout', function(req, res) {
